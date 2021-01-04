f9c19af43f4aa39eb50a72b491a44ea8faaf37dd
Al Idian <al.idian@me.com>
2021-01-03 20:13:09 -0800

    Add formatted date string to list of posts

--- .eleventy.js
+++ .eleventy.js
@@ -1,3 +1,13 @@
+const { DateTime } = require("luxon");
+
 module.exports = eleventyConfig => {
+    eleventyConfig.addFilter("htmlDateString", dateObj => {
+        return DateTime.fromJSDate(dateObj, { zone: "utc" }).toFormat("yyyy-LL-dd");
+    });
+
+    eleventyConfig.addFilter("displayDateString", dateObj => {
+        return DateTime.fromJSDate(dateObj, { zone: "utc" }).toFormat("yyyy.LL.dd");
+    });
+
     eleventyConfig.setDataDeepMerge(true);
 };
