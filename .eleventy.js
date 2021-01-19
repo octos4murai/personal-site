e2665c137e002853ce73c5d9e8ed98c247d55cf4
Al Idian <al.idian@me.com>
2021-01-18 21:11:04 -0800

    Customize Markdown processor

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,9 @@
 const { DateTime } = require("luxon");
 const syntaxHighlight = require("@11ty/eleventy-plugin-syntaxhighlight");
 
+const markdownItSup = require("markdown-it-sup");
+const markdownIt = require("markdown-it");
+
 module.exports = eleventyConfig => {
     eleventyConfig.addFilter("htmlDateString", dateObj => {
         return DateTime.fromJSDate(dateObj, { zone: "utc" }).toFormat("yyyy-LL-dd");
@@ -15,4 +18,7 @@ module.exports = eleventyConfig => {
     eleventyConfig.addPlugin(syntaxHighlight);
 
     eleventyConfig.setDataDeepMerge(true);
+
+    eleventyConfig.setLibrary("md", markdownIt({ html: true, typographer: true })
+        .use(markdownItSup));
 };
